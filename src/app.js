2c87303687f9a8a37205fa9ff9b5ee0778d96cfb
Matthew Giacomazzo <g@coplex.com>
2018-02-28 15:02:14 -0700

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,29 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({
-  description: 'Water Bill',
-  amount: 4500,
-  createdAt: 0
-}));
-
-store.dispatch(addExpense({
-  description: 'Gas Bill',
-  amount: 0,
-  createdAt: 1000
-}));
-
-store.dispatch(addExpense({
-  description: 'Rent Bill',
-  amount: 109500,
-  createdAt: 0
-}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter/>
